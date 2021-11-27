c3beb13c10c1d464d8b1dc2a5f496d792cad5a32
nhuquynhle22 <nhule22@gmail.com>
2021-11-26 23:15:36 -0500

    simple map

--- Simple_Map/js/config.js
+++ Simple_Map/js/config.js
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 // API key
+=======
+// API key
+>>>>>>> 4c7aab9fdf9ade754963f2606c408a91407a08a7
 const API_KEY = "pk.eyJ1Ijoibmh1cXV5bmhsZSIsImEiOiJja3c0OHdjOXMwMDBlMnBvMGw1NzRybWRxIn0.gmqozO5SqGXYYPfEpFgTkw";
